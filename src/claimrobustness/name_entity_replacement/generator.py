f8f5bf77e418ab251c267258a7918f2aa0e14f48
Jabez Magomere <jaybezdon@gmail.com>
2024-12-11 14:47:51 +0000

    Add async implementation and rewrite for named replacement substitutions

--- src/claimrobustness/name_entity_replacement/generator.py
+++ src/claimrobustness/name_entity_replacement/generator.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 from groq import Groq
-from openai import OpenAI
+from openai import OpenAI, RateLimitError, AsyncOpenAI
 from claimrobustness import utils, defaults
 from tqdm import tqdm
 from ratelimit import limits, sleep_and_retry
@@ -10,12 +10,38 @@ from time import sleep
 import stanza
 import configparser
 import pandas as pd
-import json
+import asyncio
+import backoff
+import jsonlines
 
 tqdm.pandas()
 
 
-def run():
+@backoff.on_exception(backoff.expo, RateLimitError)
+async def request_llm(client, prompt, model_name, temparature):
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an intelligent social media user.",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        model=model_name,
+        temperature=temparature,
+        # Streaming is not supported in JSON mode
+        stream=False,
+        # Enable JSON mode by setting the response format --> disabled for the moment
+        # Some papers have shown that json formatting can impact model generations
+        # response_format={"type": "json_object"},
+    )
+    return chat_completion.choices[0].message.content
+
+
+async def run():
     # Parse the arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("experiment_path", type=str, help="path where config lies")
@@ -35,10 +61,9 @@ def run():
 
     model_name = config["model"].get("model_string")
     temparature = config["model"].getfloat("temperature")
-    prompt_template = config["model"].get("prompt_template")
+    prompt_template_baseline = config["model"].get("prompt_template_baseline")
+    prompt_template_worstcase = config["model"].get("prompt_template_worstcase")
 
-    baseline = config["generation"].getint("baseline")
-    worstcase = config["generation"].getint("worstcase")
     samples = config["generation"].getint("number_of_samples")
 
     # Load the test data used for generating misinformation edits
@@ -59,120 +84,71 @@ def run():
     print("Cleaning the tweet query")
     run_queries["query"] = run_queries["query"].progress_apply(utils.clean_tweet)
 
-    # Identify named entity tokens to replace using Stanza for NER
-    def parse_ner(input_text, nlp: stanza.models, types_to_exclude=[]):
-        doc = nlp(input_text)
-        entities = [
-            {
-                "token": ent.text,
-                "type": ent.type,
-                "description": defaults.NAMED_ENTITIES_TYPE_DEFINITIONS.get(
-                    ent.type, "Unknown"
-                ),
-            }
-            for sent in doc.sentences
-            for ent in sent.ents
-            if ent.type not in types_to_exclude
-        ]
-        return json.dumps(entities)
-
-    nlp = stanza.Pipeline(lang="en", processors="tokenize,ner")
-    run_queries["tokens_to_replace"] = run_queries["query"].progress_apply(
-        lambda query: parse_ner(query, nlp)
-    )
-
     if "gpt" in model_name:
-        client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+        client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
     elif "llama" in model_name:
         client = Groq(
             api_key=os.environ["GROQ_API_KEY"],
         )
 
-    @sleep_and_retry
-    @limits(calls=500, period=timedelta(seconds=60).total_seconds())
-    def get_ner_replacements(
-        client,
-        prompt_template: str,
-        claim: str,
-        fact_check: str,
-        named_entities: list,
-        budget: int,
-    ):
-        max_retries = 3
-        retries = 0
-        while retries < max_retries:
-            try:
-                prompt = prompt_template.format(
-                    number_of_samples=samples,
-                    claim=claim,
-                    fact_check=fact_check,
-                    named_entities=named_entities[:budget],
-                )
-                chat_completion = client.chat.completions.create(
-                    messages=[
-                        {
-                            "role": "system",
-                            "content": "You are an intelligent social media user.",
-                        },
-                        {
-                            "role": "user",
-                            "content": prompt,
-                        },
-                    ],
-                    model=model_name,
-                    temperature=temparature,
-                    # Streaming is not supported in JSON mode
-                    stream=False,
-                    # Enable JSON mode by setting the response format
-                    response_format={"type": "json_object"},
-                )
-                res = chat_completion.choices[0].message.content
-
-                return res
-            except Exception as e:
-                retries += 1
-                if retries < max_retries:
-                    print(
-                        f"Retrying ({retries}/{max_retries}) after BadRequestError: {e}"
-                    )
-                    sleep(2)  # Wait for 2 seconds before retrying
-                else:
-                    print(
-                        f"Failed after {max_retries} retries. Last BadRequestError: {e}"
-                    )
-                    raise  # Raise the exception after max retries
-
-    def process_queries(queries: pd.DataFrame, budget: int, type: str):
-        df = queries[queries["tokens_to_replace"].apply(len) >= budget]
-        df.loc[:, "replaced_token"] = df.progress_apply(
-            lambda row: get_ner_replacements(
-                client=client,
-                prompt_template=prompt_template,
-                claim=row["query"],
-                fact_check=row["target"],
-                named_entities=json.loads(row["tokens_to_replace"]),
-                budget=budget,
-            ),
-            axis=1,
-        )
+    async def process_queries(queries: pd.DataFrame, budget: str):
+        df = queries.copy()
+        if budget == "baseline":
+            prompt_template = prompt_template_baseline
+        else:
+            prompt_template = prompt_template_worstcase
 
         # Save the file output
         save_dir = f"{args.experiment_path}/{dataset}"
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
-        df.to_csv(
-            os.path.join(save_dir, f"{type}_named_entity_replacements.csv"), index=False
+        # Define jsonl file to save the generated edits
+        output_file = os.path.join(
+            save_dir, f"{budget}_named_entity_replacements.jsonl"
         )
 
-        print(f"Saved {type} named entity replacements to {save_dir}")
+        # Load the existing ids
+        existing_ids = set()
+        with open(output_file, "a+", encoding="utf-8") as f:
+            f.seek(0)
+            try:
+                with jsonlines.Reader(f) as reader:
+                    data = list(reader.iter())
+                for obj in data:
+                    existing_ids.add(obj["query_id"])
+            except jsonlines.jsonlines.InvalidLineError:
+                # This will be raised if the file is empty. You can handle it as you need.
+                pass
+
+        filtered_df = df[~df["query_id"].isin(existing_ids)]
+        print(f"Generating rewrites for {len(filtered_df)} tweets")
+
+        with jsonlines.open(output_file, mode="a") as writer:
+            for _, row in tqdm(filtered_df.iterrows()):
+                prompt = prompt_template.format(
+                    number_of_samples=samples,
+                    claim=row["query"],
+                    fact_check=row["target"],
+                )
+                llm_response = await request_llm(
+                    client, prompt, model_name, temparature
+                )
+
+                json_obj = {
+                    "query_id": row["query_id"],
+                    "query": row["query"],
+                    "target": row["target"],
+                    "rewrites": llm_response,
+                }
+                writer.write(json_obj)
 
     if not args.no_baseline:
-        process_queries(run_queries, baseline, "baseline")
+        await process_queries(run_queries, "baseline")
 
     if not args.no_worstcase:
-        process_queries(run_queries, worstcase, "worstcase")
+        await process_queries(run_queries, "worstcase")
 
 
 if __name__ == "__main__":
-    run()
+    asyncio.run(run())
